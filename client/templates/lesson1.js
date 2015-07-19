4709624de28074714981b777dbfcbd174ca24946
johnsimer <johnsimerlink@gmail.com>
2015-07-19 11:29:15 -0400

    layout for esson1 teaching

--- client/templates/lesson1.js
+++ client/templates/lesson1.js
@@ -1,4 +1,96 @@
+Session.set('teach', true);
+Session.set('test', false);
+
+Session.set('cardNum', 0);
 Session.set('questionNum', '0');
+
+var teachingCards = [
+	{
+		type: 'noun',
+		content: {
+			image: 'img/man.jpg',
+			speak: 'el hombre',
+			title: 'el hombre',
+			text: 'The word for "man" in Spanish is "hombre". To say "the man", say "el hombre".',
+			english: 'man',
+			spanish: 'hombre',
+		},
+		facts: [
+			{
+				category: 'nouns'
+				name:'hombre',
+				english: 'man',
+				spanish: 'hombre'
+			}
+		],
+		skills: [
+			{
+				category: 'sentenceForming'
+				name: 'articles-masculine',
+				english: 'man',
+				spanish: 'hombre'
+			}
+
+		]
+	},
+	{
+		type: 'noun',
+		content: {
+			image: 'img/car.jpg',
+			speak: 'el auto',
+			title: 'el auto',
+			english: 'car',
+			spanish: 'auto',
+		},
+		facts: [
+			{
+				category: 'nouns'
+				name:'auto',
+				english: 'car',
+				spanish: 'auto'
+			}
+		]
+	},
+	{
+		type: 'verb',
+		content: {
+			image: 'img/heart.jpg',
+			speak: 'ama',
+			title: 'ama',
+			english: '(he/she/it/[a noun] loves',
+			text: '"[a noun] loves" is "ama", where [a noun] would be for example "hombre". So to say "the man loves the car", you would say "el hombre ama el auto"',
+			spanish: 'ama',
+		},
+		facts: [
+			{
+				introductory: true,
+				category: 'verbs'
+				name:'ama',
+				english: 'loves',
+				spanish: 'ama'
+			}
+		],
+		skills: [
+			{
+				category: 'first-conjugation-present',
+				name: '3rd-singular'
+			},
+			{
+				category: 'sentenceForming',
+				name: 'simple-sentence'
+			},
+			{
+				category: 'sentenceForming',
+				name: 'directObject'
+			}
+
+		]
+	}
+
+];
+
+
+
 var questions = [
 	{
 		question: "Write out \"man\" in Spanish",
@@ -12,7 +104,16 @@ var questions = [
 	}
 ];
 
-Template.quickQuiz1.helpers({
+Template.lesson1.helpers({
+	teach: function() {
+		return Session.get('teach');
+	},
+	test: function(){
+		return Session.get('test');
+	}
+});
+
+Template.test1.helpers({
 	question: function() {
 		//return "hello";
 
@@ -22,7 +123,7 @@ Template.quickQuiz1.helpers({
 
 });
 
-Template.quickQuiz1.events({
+Template.test1.events({
 	'click #checkAnswer': function(){
 
 		var correct = $('#userAnswer').val() == questions[Session.get('questionNum')].answer;
@@ -31,12 +132,16 @@ Template.quickQuiz1.events({
 
 		//--STORE INTERACTION
 
-		addPoints(10); // simple 10 for now.
-		pointsAnimation(10);
+		if (correct){
 
-		Session.set('questionNum', +Session.get('questionNum') + 1);
+			addPoints(10); // simple 10 for now.
+			pointsAnimation(10);
 
-		if (correct){}
+			Session.set('questionNum', +Session.get('questionNum') + 1);
+
+			
+		}
+
 
 	}
 });
